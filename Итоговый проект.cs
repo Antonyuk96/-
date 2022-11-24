6d583b20f50216ed7c192db2b315fea26dc7681c
Анатолий <racerchampion@mail.ru>
2022-11-24 21:28:30 +0300

    Сохраняем изменения

---
+++ Итоговый проект.cs
@@ -0,0 +1,24 @@
+﻿string[] array1 = new string[5] {"США", "КНР", "Кения", "Россия", "ЮАР"};
+string[] array2 = new string[array1.Length];
+void ArrayIf(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void Print(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArrayIf(array1, array2);
+Print(array2);
